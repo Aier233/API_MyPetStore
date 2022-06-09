ec3742a3310244b2dc91686e861d46ef5a5e013a
Aier233 <577414013@qq.com>
2022-06-09 18:04:14 +0800

    CORSFilter更改

--- src/main/java/org/csu/mypetstore/api/controller/front/CORSFilter.java
+++ src/main/java/org/csu/mypetstore/api/controller/front/CORSFilter.java
@@ -3,6 +3,7 @@ package org.csu.mypetstore.api.controller.front;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -14,10 +15,11 @@ public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {
         HttpServletResponse response = (HttpServletResponse) res;
-        //使用xhrFields后Access-Control-Allow-Origin
-        response.setHeader("Access-Control-Allow-Origin","http://localhost:8888");
-        //使用xhrFields前的Access-Controller-Allow-Orgin
-        //response.setHeader("Access-Controller-Allow-Orgin","*");
+        HttpServletRequest request   = (HttpServletRequest) req;
+        //使用xhrFields后的Access-Control-Allow-Origin
+        response.setHeader("Access-Control-Allow-Origin",request.getHeader("Origin"));
+        //使用xhrFields前的Access-Controller-Allow-Origin
+        //response.setHeader("Access-Control-Allow-Origin","*");
         response.setHeader("Access-Control-Allow-Credentials","true");
         response.setHeader("Access-Control-Allow-Methods","POST,GET,PATCH,DELETE,PUT");
         response.setHeader("Access-Control-Max-Age","3600");
